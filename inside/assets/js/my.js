8d5a1ddc9182eebe27302f93b3cdfc24b88464d6
aangel3569 <aangel3569@gmail.com>
2019-08-08 11:08:52 +0800

    Unit 0

--- inside/assets/js/my.js
+++ inside/assets/js/my.js
@@ -1,3 +1,31 @@
-$(function(){
-    
+$(function () {
+
+    var arr = ["u1/U1-1.png", "u1/U1-2.png", "u1/U1-3.png", "u1/U1-4.png", "u1/U1-5.png", "u1/U1-6.png"]
+    var src = $(".classp").attr("src");
+    var index = arr.indexOf(src);
+
+    $(".a2").click(function () {
+
+        if (index < 5) {
+            index += 1;
+            $(".classp").fadeOut(function () {
+                $(".classp").attr("src", arr[index]);
+            });
+            $(".classp").fadeIn();
+        }
+
+    });
+
+    $(".a1").click(function () {
+
+        if (index > 0) {
+            index -= 1;
+            $(".classp").fadeOut(function () {
+                $(".classp").attr("src", arr[index]);
+            });
+            $(".classp").fadeIn();
+        }
+        
+    });
+
 });
